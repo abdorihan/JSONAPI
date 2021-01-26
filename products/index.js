72dc74a062fd497c099b015d43ee53f31e4a5241
abdorihan <abdorihan@yahoo.com>
2021-01-26 13:53:48 +0200

    Create index.js

---
+++ products/index.js
@@ -0,0 +1,16 @@
+[
+{
+   'name': 'رائحة كرت عادية',
+   'quantity': 12,
+   'type': 'روائح',
+   'buyPrice': 1.25,
+   'price': 2
+ },
+ {
+   'name': 'رائحة كرت L.D',
+   'quantity': 10,
+   'type': 'روائح',
+   'buyPrice': 2.5,
+   'price': 4
+ }
+]
